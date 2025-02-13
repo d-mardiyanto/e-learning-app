0dc13ce38312257654d0ca55bce4beed800373f9
Yudha <herlambang6661@gmail.com>
2025-02-13 22:22:13 +0700

    modified:   src/api/api_program_study.ts

--- src/api/api_program_study.ts
+++ src/api/api_program_study.ts
@@ -1,37 +1,37 @@
 import apiClient from "./apiClient";
 
-// Define ProgramStudy type (optional)
-interface ProgramStudy {
+// Define ProgramName type (optional)
+interface ProgramName {
     id: string;
     class_name: string;
 }
 
 // Get all ProgramStudys
-export const getProgramStudy = async () => {
-    const response = await apiClient.get("/api/prody/show");
-    return response.data.programStudy;
+export const getProgramStudy = async (): Promise<ProgramName[]> => {
+    const response = await apiClient.get<{ programname: ProgramName[] }>("/api/prody/show");
+    return response.data.programname;
 };
 
 // Get a single ProgramStudy by ID
 export const getProgramStudyById = async (id: number) => {
-    const response = await apiClient.get(`/api/prody/${id}`);
-    return response.data.programStudy;
+    const response = await apiClient.get<{programname: ProgramName[] }>(`/api/prody/${id}`);
+    return response.data.programname;
 };
 
 // Create a new ProgramStudy
-export const createProgramStudy = async (data: ProgramStudy) => {
+export const createProgramStudy = async (data: ProgramName) => {
     const response = await apiClient.post("/api/prody", data);
-    return response.data.programStudy;
+    return response.data.programname;
 };
 
 // Update an ProgramStudy
-export const updateProgramStudy = async (id: number, data: ProgramStudy) => {
+export const updateProgramStudy = async (id: number, data: ProgramName) => {
     const response = await apiClient.put(`/api/prody/${id}`, data);
-    return response.data.programStudy;
+    return response.data.programname;
 };
 
 // Delete an ProgramStudy
 export const deleteProgramStudy = async (id: number) => {
     const response = await apiClient.delete(`/api/prody/${id}`);
-    return response.data.programStudy;
+    return response.data.programname;
 };
